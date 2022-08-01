06b39ae96d0186dc62812ad5a38c3b68547ba3ee
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-08-01 14:40:10 -0400

    initial component/route structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Game from "./components/Game";
+import Nav from "./components/Nav";
+import Leaderboard from "./components/Leaderboard";
+import Home from "./components/Home";
+import Social from "./components/Social";
+import "./styles/app.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="leaderboard" element={<Leaderboard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/:id" element={<Game />} />
+      </Routes>
+      <Social />
+    </Router>
   );
-}
+};
 
 export default App;
