f70edc6ceebb7d916226f18de95e7b9657916563
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-08-04 17:35:38 -0400

    added local game array

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,11 @@ import React from "react";
 import GameSelection from "./GameSelection";
 
 export const Home = () => {
-  return <GameSelection />;
+  const games = [
+    { image: "imageURL", gameName: "game1" },
+    { image: "imageURL", gameName: "game2" },
+    { image: "imageURL", gameName: "game3" },
+  ];
+  return <GameSelection games={games} />;
 };
 export default Home;
