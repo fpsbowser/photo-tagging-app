16068d78d035401a7a36481bd6e2c9a0b92055ba
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-08-04 17:36:07 -0400

    refactored to include props

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-export const Leaderboard = () => {
+export const Leaderboard = (props) => {
+  const { gameInfo } = props;
+
   return (
     <div>
       <h1>Leaderboard</h1>
+      <div className="game-info">{gameInfo}</div>
     </div>
   );
 };
