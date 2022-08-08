1c3decaa17538306c2600c5315f1afd0f7100063
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-08-08 17:34:09 -0400

    initial functionality created

--- src/components/Endgame.js
+++ src/components/Endgame.js
@@ -1,32 +1,95 @@
-import React from "react";
+import { addDoc, collection } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import "../styles/endgame.css";
+import { database } from "./firebase";
+import Leaderboard from "./Leaderboard";
 
 const Endgame = (props) => {
   const { gameboard, time } = props;
+  const [inputValue, setInputValue] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
-  let seconds = Math.round(time / 1000);
-  let minutes = Math.round(seconds / 60);
+  useEffect(() => {
+    setSubmitted(false);
+  }, []);
+
+  const convertTime = () => {
+    let minutes = Math.floor(time / 60000);
+    let seconds = ((time % 60000) / 1000).toFixed(0);
+    return seconds === 60
+      ? minutes + 1 + ":00"
+      : minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  };
+
+  let correctTime = convertTime(time);
   let game;
 
+  const update = (e) => {
+    e.preventDefault();
+    let levelCollection;
+    if (inputValue === "") {
+      return;
+    }
+    if (gameboard === "game1") {
+      levelCollection = "level-one-leaderboard";
+    } else if (gameboard === "game2") {
+      levelCollection = "level-two-leaderboard";
+    } else {
+      levelCollection = "level-three-leaderboard";
+    }
+    const levelOneLeaderboard = collection(database, levelCollection);
+    addDoc(levelOneLeaderboard, { name: inputValue, time: correctTime })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+
+    setInputValue("");
+    setSubmitted(true);
+  };
+
   if (gameboard === "game1") {
-    game = "gameboard one";
+    game = "level one";
   } else if (gameboard === "game2") {
-    game = "gameboard two";
+    game = "level two";
   } else {
-    game = "gameboard three";
+    game = "level three";
   }
-  return (
-    <div className="endgame-container">
-      <div id="finish-text">
-        You finished {game}, with a time of {minutes}:{seconds}!
+
+  if (submitted) {
+    return <Leaderboard />;
+  } else {
+    return (
+      <div className="endgame-container">
+        <div className="form-container">
+          <div id="finish-text">
+            You finished {game}, with a time of {correctTime}!
+          </div>
+          <div id="end-form">
+            If you want to publish your time to the leaderboard please enter
+            your name below
+          </div>
+          <div className="input-container">
+            <form onSubmit={update}>
+              <input
+                id="name-input"
+                placeholder="Enter name"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              {/* <Link to={"/leaderboard"}> */}
+              <button id="publish-button" type="submit">
+                Publish
+              </button>
+              {/* </Link> */}
+            </form>
+          </div>
+        </div>
       </div>
-      <div id="end-form">Would you like to publish to leaderboard?</div>
-      <div className="button-container">
-        <button>Yes</button>
-        <button>No</button>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Endgame;
--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,12 +1,107 @@
-import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import "../styles/leaderboard.css";
+import { database } from "./firebase";
 
-export const Leaderboard = (props) => {
-  const { gameInfo } = props;
+export const Leaderboard = () => {
+  const [levelOneLeaderboard, setLevelOneLeaderboard] = useState([]);
+  const [levelTwoLeaderboard, setLevelTwoLeaderboard] = useState([]);
+  const [levelThreeLeaderboard, setLevelThreeLeaderboard] = useState([]);
+
+  const fetchLeaderboardOne = async () => {
+    const leaderboardCollection = collection(database, "level-one-leaderboard");
+    const leaderboardSnapshot = await getDocs(leaderboardCollection);
+    const fetchedLeaderboard = leaderboardSnapshot.docs.map((doc) => ({
+      data: doc.data(),
+      id: doc.id,
+    }));
+    setLevelOneLeaderboard(fetchedLeaderboard);
+  };
+
+  const fetchLeaderboardTwo = async () => {
+    const leaderboardCollection = collection(database, "level-two-leaderboard");
+    const leaderboardSnapshot = await getDocs(leaderboardCollection);
+    const fetchedLeaderboard = leaderboardSnapshot.docs.map((doc) => ({
+      data: doc.data(),
+      id: doc.id,
+    }));
+    setLevelTwoLeaderboard(fetchedLeaderboard);
+  };
+
+  const fetchLeaderboardThree = async () => {
+    const leaderboardCollection = collection(
+      database,
+      "level-three-leaderboard"
+    );
+    const leaderboardSnapshot = await getDocs(leaderboardCollection);
+    const fetchedLeaderboard = leaderboardSnapshot.docs.map((doc) => ({
+      data: doc.data(),
+      id: doc.id,
+    }));
+    setLevelThreeLeaderboard(fetchedLeaderboard);
+  };
+
+  useEffect(() => {
+    fetchLeaderboardOne();
+    fetchLeaderboardTwo();
+    fetchLeaderboardThree();
+  }, []);
 
   return (
-    <div>
-      <h1>Leaderboard</h1>
-      <div className="game-info">{gameInfo}</div>
+    <div className="leaderboard-container">
+      <div className="leaderboard">
+        <div className="level-one">
+          <h1 id="leaderboard-header">Level One</h1>
+          <div id="name-header">Name:</div>
+          <div id="time-header">Time:</div>
+          {/* loop through database and return leaderboard */}
+          <div className="entry-container">
+            {levelOneLeaderboard.map((entry, i) => {
+              return (
+                <div className="entry" key={entry.id}>
+                  <div id="entry-number">{i + 1}</div>
+                  <div id="entry-name">{entry.data.name}</div>
+                  <div id="entry-time">{entry.data.time}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="level-two">
+          <h1 id="leaderboard-header">Level Two</h1>
+          <div id="name-header">Name:</div>
+          <div id="time-header">Time:</div>
+          {/* loop through database and return leaderboard */}
+          <div className="entry-container">
+            {levelTwoLeaderboard.map((entry, i) => {
+              return (
+                <div className="entry" key={entry.id}>
+                  <div id="entry-number">{i + 1}</div>
+                  <div id="entry-name">{entry.data.name}</div>
+                  <div id="entry-time">{entry.data.time}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="level-three">
+          <h1 id="leaderboard-header">Level Three</h1>
+          <div id="name-header">Name:</div>
+          <div id="time-header">Time:</div>
+          {/* loop through database and return leaderboard */}
+          <div className="entry-container">
+            {levelThreeLeaderboard.map((entry, i) => {
+              return (
+                <div className="entry" key={entry.id}>
+                  <div id="entry-number">{i + 1}</div>
+                  <div id="entry-name">{entry.data.name}</div>
+                  <div id="entry-time">{entry.data.time}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
